516ed93c5be1e87c28cfaf92adf05502c231dfb1
Mahliyo <gasandigital13@gmail.com>
2026-05-17 23:03:17 +0500

    headerlayout

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -10,9 +10,9 @@ function Header() {
     <div>
         <h1>logo</h1>
         <ul className="header-list">
-            <li><Link   to="#">About</a></li>
-            <li><Link   to="#">Contact</a></li>
-            <li><Link   to="#">Services</a></li>
+            <li><Link to="about">About</Link></li>
+            <li><Link to="contact">Contact</Link></li>
+            <li><Link to="services">Services</Link></li>
         </ul>
     </div>
   )
