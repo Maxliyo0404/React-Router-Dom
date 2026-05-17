24f3026d6770ca7086ca72999a7aa49af1a5e288
Mahliyo <gasandigital13@gmail.com>
2026-05-17 23:26:17 +0500

    headerlayout

--- src/pages/ContactPages/ContactPages.jsx
+++ src/pages/ContactPages/ContactPages.jsx
@@ -5,7 +5,7 @@ import contact_1 from "./image/contact_1.jpg"
 function ContactPages() {
   return (
    <div className="cards">
-          <img src={contact_1} alt="rasm" />
+          <img className="rasm" src={contact_1} alt="rasm" />
           <h1 className='title'>ContactPages</h1>
           <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ea expedita possimus voluptas veritatis mollitia modi, vel dolore tempore eum quam exercitationem velit sapiente dignissimos odit voluptates quaerat ab voluptatum?</p>
       </div>
--- src/pages/ServicesPages/ServicesPages.jsx
+++ src/pages/ServicesPages/ServicesPages.jsx
@@ -4,8 +4,8 @@ import services_1 from "./image/services_1.jpg"
 
 function ServicesPages() {
   return (
-    <div>
-           <img src={services_1} alt="rasm" />
+    <div className="cards">
+           <img  classNamerasmsrc={services_1} alt="rasm" />
            <h1 className='title'>ServicesPages</h1>
            <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ea expedita possimus voluptas veritatis mollitia modi, vel dolore tempore eum quam exercitationem velit sapiente dignissimos odit voluptates quaerat ab voluptatum?</p>
        </div>
