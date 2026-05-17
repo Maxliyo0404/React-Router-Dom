198bc84a2494d8a714aed2041f7ba4a419754663
Mahliyo <gasandigital13@gmail.com>
2026-05-17 21:03:19 +0500

    React-Router-Dom about

---
+++ src/pages/ContactPages/ContactPages.jsx
@@ -0,0 +1,15 @@
+import "./ContactPages.css";
+import React from 'react'
+import contact_1 from "./image/contact_1.jsx"
+
+function ContactPages() {
+  return (
+   <div>
+          <img src={contact_1} alt="rasm" />
+          <h1 className='title'>ContactPages</h1>
+          <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ea expedita possimus voluptas veritatis mollitia modi, vel dolore tempore eum quam exercitationem velit sapiente dignissimos odit voluptates quaerat ab voluptatum?</p>
+      </div>
+  )
+}
+
+export default ContactPages
