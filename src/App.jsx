1029aa0d4ee35caf6658038759240d1fb0dfd944
Mahliyo <gasandigital13@gmail.com>
2026-05-17 22:48:33 +0500

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from './Components/Layout/Layout'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Layout/>
+    
     </div>
   )
 }
