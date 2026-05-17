fd9648ab109fb7c4ecec022590e27cdc4ede5d09
Mahliyo <gasandigital13@gmail.com>
2026-05-17 23:24:43 +0500

    headerlayout

--- src/pages/AboutPages/AboutPages.jsx
+++ src/pages/AboutPages/AboutPages.jsx
@@ -3,8 +3,8 @@ import abut_1 from "./image/abut_1.jpg";
 import "./AboutPages.css"
 function AboutPages() {
   return (
-    <div>
-        <img src={abut_1} alt="rasm" />
+    <div className='cards'>
+        <img className='rasm' src={abut_1} alt="rasm" />
         <h1 className='title'>AboutPages</h1>
         <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ea expedita possimus voluptas veritatis mollitia modi, vel dolore tempore eum quam exercitationem velit sapiente dignissimos odit voluptates quaerat ab voluptatum?</p>
     </div>
