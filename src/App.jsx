45afe92ba2ace1528cd19470fb85f2b2784ace76
Mahliyo <gasandigital13@gmail.com>
2026-05-17 22:55:39 +0500

    headerlayout

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import Layout from './Components/Layout/Layout'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AboutPages from './pages/AboutPages/AboutPages'
-import
+import ContactPages from './pages/ContactPages/ContactPages'
+import ServicesPages from './pages/ServicesPages/ServicesPages'
 
 function App() {
   return (
     <div>
     <BrowserRouter>
     <Routes>
-      <Route  path='about' element={<} />
+      <Route  path='about' element={<AboutPages/>} />
     </Routes>
     </BrowserRouter>
     </div>
