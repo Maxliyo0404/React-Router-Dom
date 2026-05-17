62a6e6a911824f217888b24e8e09498ae8c44d5e
Mahliyo <gasandigital13@gmail.com>
2026-05-18 00:04:53 +0500

    headerlayout  outlet fnish

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 function Footer() {
   return (
      <div className="boxs">
-        <h1 className="footer-title">Lorem </h1>
+        <h1 className="footer-title">Footer</h1>
         <ul className="footer-list">
             <li className="footer-item"><Link className="link" to="about">About</Link></li>
             <li className="footer-item"><Link className="link" to="contact">Contact</Link></li>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,7 +8,7 @@ import React from 'react';
 function Header() {
   return (
     <div className="boxs">
-        <h1 className="header-title">Lorem </h1>
+        <h1 className="header-title">Logo </h1>
         <ul className="header-list">
             <li className="header-item"><Link className="link" to="about">About</Link></li>
             <li className="header-item"><Link className="link" to="contact">Contact</Link></li>
