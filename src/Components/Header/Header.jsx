f7a6d2778319b9eb750b7c7388e453edee72faff
Mahliyo <gasandigital13@gmail.com>
2026-05-17 23:50:16 +0500

    headerlayout  outlet fnish

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,7 +8,7 @@ import React from 'react';
 function Header() {
   return (
     <div className="boxs">
-        <h1 className="header-title">logo</h1>
+        <h1 className="header-title">Lorem ipsum dolor sit.</h1>
         <ul className="header-list">
             <li className="header-item"><Link className="link" to="about">About</Link></li>
             <li className="header-item"><Link className="link" to="contact">Contact</Link></li>
